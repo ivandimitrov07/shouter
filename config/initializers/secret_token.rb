f37ab16f0a059bd69141433dda1c36f0bcb8e5fb
ivandimitrov07 <idimitrov07@yahoo.com>
2013-11-17 18:23:27 +0200

    initial commit:twitter colone app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = 'a53998ae1f0f152974cb1aba18516dbe4ab3e638755afadbcf32792d98dd42e0937c0820b2cd545042586b4c10adb8d5d79392076c258f3a739f3414a05c7f0d'
